b91316e9e31504aa4e86429579d8cb285fa53a27
Kazem Mirzaei <k90mirzaei@gmail.com>
2022-08-22 00:52:43 +0430

    resolve some issue

--- src/utils/nodeFinder.js
+++ src/utils/nodeFinder.js
@@ -3,9 +3,8 @@ import { keyExists } from "./highliter";
 let collections = [];
 
 const iterate = (search, node) => {
-  if (node.hasChildren) {
+  if (node.children) {
     for (const key in node.children) {
-      console.log(node.children[key] instanceof Array);
       const result = iterate(search, node.children[key]);
 
       if (result !== undefined) {
@@ -17,7 +16,9 @@ const iterate = (search, node) => {
     }
   }
   // check is current node code same as code then return it else decrement index by one
-  if (keyExists(search, node.code) || keyExists(search, node.description)) return node.code;
+  if (keyExists(search, node.code) !== false || keyExists(search, node.description) !== false) return node.code;
+
+  console.log(search, node.code, keyExists(search, node.code));
 
   return;
 };
