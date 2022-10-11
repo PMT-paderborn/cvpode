a7a961ac4c39f04894df487331ca98b154e6ec7d
Kazem Mirzaei <k90mirzaei@gmail.com>
2022-10-11 23:17:34 +0800

    feat: add data event

--- src/components/FormField/FormField.js
+++ src/components/FormField/FormField.js
@@ -1,32 +1,15 @@
-import { InputBase, Typography } from "@mui/material";
 import { useState } from "react";
 import styles from "./FormField.module.css";
 import SearchCodes from "./SearchCodes";
-import { getAllCache, removeCache, clearCache } from "../../services/cacheService";
 
 const FormField = () => {
   const [open, setOpen] = useState(false);
-  const [caches, setCaches] = useState(getAllCache());
 
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>1.2) CPV-CODE</h2>
       <div className={styles.subtitle}>
         <p>CPV-CODE Hauptteil</p>(<span onClick={() => setOpen(true)}>Liste</span>)
-      </div>
-      <div className={styles.inputGroup}>
-        {caches?.length === 0 && (
-          <InputBase id="input-text-input" placeholder="3412100" className={styles.input} />
-        )}
-        {caches?.length > 0 && (
-          <ul>
-            {caches.map((item) => (
-              <li key={item.code} title={item.code + " - " + item.description}>
-                {item.code}
-              </li>
-            ))}
-          </ul>
-        )}
       </div>
       <SearchCodes open={open} setOpen={setOpen} />
     </div>
--- src/components/FormField/SearchCodes.js
+++ src/components/FormField/SearchCodes.js
@@ -1,20 +1,28 @@
-import { Box, Button, IconButton, Modal, Typography } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
+import { Box, Button, Modal, Typography } from "@mui/material";
 import styles from "./FormField.module.css";
 import Search from "../../screens/Search";
+import { getAllCache } from "../../services/cacheService";
 
 const SearchCodes = ({ open, setOpen }) => {
+  const confirmIt = () => {
+    const event = new CustomEvent("selectedCpvCode", {
+      detail: getAllCache(),
+    });
+    window.dispatchEvent(event);
+    setOpen(false);
+  };
+
   return (
     <Modal open={open} onClose={() => setOpen(false)}>
       <div className={styles.modal}>
-        <Box sx={{ display: "flex", paddingBottom: 2 }}>
+        <Box sx={{ display: "flex", paddingBottom: 0 }}>
           <Typography fontWeight={600} fontSize={24} flex={1}>
             Selected CPV Codes
           </Typography>
         </Box>
         <Search />
         <Box sx={{ display: "flex", justifyContent: "end", paddingTop: 4 }}>
-          <Button variant="contained" onClick={() => setOpen(false)}>
+          <Button variant="contained" onClick={confirmIt}>
             Confirm
           </Button>
         </Box>
